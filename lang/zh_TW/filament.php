8f151ad05e3f8ad0c619b1dc1f1894f2ec0c0ffe
Patrick Her <renfu.her@gmail.com>
2025-01-14 15:25:17 +0800

    first commit

---
+++ lang/zh_TW/filament.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'pages' => [
+        'list' => [
+            'title' => '列表',
+        ],
+        'create' => [
+            'title' => '新增',
+        ],
+        'edit' => [
+            'title' => '編輯',
+        ],
+    ],
+    'common' => [
+        'new' => '新增',
+        'no' => '無',
+    ],
+];
